c5c4976f01d8d672c77ccb5df0c272bff48406de
adnan <sthep90@gmail.com>
2025-06-16 16:54:11 +0600

    code improved

--- src/main.cpp
+++ src/main.cpp
@@ -36,10 +36,8 @@ int main() {
         glBufferData(GL_ARRAY_BUFFER,sizeof(float)*18,vertex_data,GL_STATIC_DRAW);
         glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,sizeof(float)*3,(void*)0);
         glEnableVertexAttribArray(0);
-        if (glIsVertexArray(vao))glBindVertexArray(vao);
+        if (glIsVertexArray(vao)) glBindVertexArray(vao);
         glUseProgram(glx.ShaderTool().getProgram());
-
-
     });
 
 
