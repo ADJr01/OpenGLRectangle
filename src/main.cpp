21e3b79798c380fb24110a95c6b901530d9eb59a
adnan <sthep90@gmail.com>
2025-06-18 19:15:15 +0600

    few updates

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,7 @@ int main() {
         //buffer
         glGenBuffers(1,&vbo);
         glBindBuffer(GL_ARRAY_BUFFER,vbo);
-        glBufferData(GL_ARRAY_BUFFER,sizeof(float)*18,vertex_data,GL_DYNAMIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER,sizeof(float)*18,vertex_data,GL_STATIC_DRAW);
         glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,sizeof(float)*3,(void*)0);
         glEnableVertexAttribArray(0);
         if (glIsVertexArray(vao)) glBindVertexArray(vao);
